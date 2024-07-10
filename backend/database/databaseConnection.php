376351a9b81d89b07c4beee9127a4843b89e202c
rohitghatal1 <rohitghatal@gmail.com>
2024-07-10 16:17:06 +0545

    added database connection file

---
+++ backend/database/databaseConnection.php
@@ -0,0 +1,17 @@
+<?php
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "gadgetHub";
+// Create connection
+$conn = new mysqli($hostname, $username, $password, $database);
+
+if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+
+else{
+    echo "<script>console.log('Connected Successfully')</script>";
+}
+?>
