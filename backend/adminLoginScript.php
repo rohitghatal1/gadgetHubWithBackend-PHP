77da4c3119b3ca0c59500554ca04c12daaf1c2be
rohitghatal1 <rohitghatal@gmail.com>
2024-07-17 18:43:13 +0545

    added login script with backend connectivity successfully

---
+++ backend/adminLoginScript.php
@@ -0,0 +1,17 @@
+<?php 
+require 'database/databaseConnection.php';
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $username = $_POST['adminUsername'];
+    $password = $_POST['adminPassword'];
+
+    $getAdminDetails ="SELECT * FROM admin_table";
+    $fetchedAdminData = $conn->query($getAdminDetails);
+    if($fetchedAdminData->num_rows > 0){
+        while($adminData = $fetchedAdminData->fetch_assoc()){
+            if($username == $adminData['adminUsername'] && $password == $adminData['adminPassword']){
+                echo "login successful";
+            }
+        }
+    }
+}
