eea68c7dd6a46c6ca525c42b2ea126eeabfb8da5
rohitghatal1 <rohitghatal@gmail.com>
2024-07-21 14:12:50 +0545

    added php scrit for storing data into database

---
+++ backend/addMobiles.php
@@ -0,0 +1,31 @@
+<?php 
+require 'database/databaseConnection.php';
+
+if($_SERVER["REQUEST_METHOD"]== "POST"){
+
+    $target_folder = '../mobilePhotos/';
+    $target_file = $target_folder . basename($_FILES["mobilePhoto"]["name"]);
+    move_uploaded_file($_FILES["mobilePhoto"]["tmp_name"], $target_file);
+
+    $brand = $_POST["brand"];
+    $model = $_POST["model"];
+    $processor = $_POST["processor"];
+    $RAM = $_POST["RAM"];
+    $storage = $_POST["storage"];
+    $otherSpecifications = $_POST["specifications"];
+    $price = $_POST["price"];
+    $mobileImage = $target_file;
+
+    $addMobile = $conn->prepare("INSERT INTO mobiles(brand, model, processor, RAM, storage, otherSpecs, Mprice, photoPath) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+    $addMobile->bind_param("ssssssss", $brand, $model, $processor, $RAM, $storage, $otherSpecifications, $price, $mobileImage);
+
+    if($addMobile->execute() === true){
+        echo "<script>alert('Mobile added successfully')</script>";
+        echo "<script>window.location.href='adminDashboard.php';</script>";
+    }
+    else {
+        die("Error: " . $sql . "<br>" . $conn->error());
+    }
+    $addLaptop->close();
+    $conn->close();
+}
--- backend/adminDashboard.php
+++ backend/adminDashboard.php
@@ -320,24 +320,30 @@ else{
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" style="filter: invert(1);"></button>
                         </div>
                         <div class="modal-body">
-                            <form action="">
+                            <form action="addMobiles.php" method = "post" enctype = "multipart/form-data">
                                 <label class="form-label mt-2">Photo</label>
-                                <input type="file" accept =".jpg, .png, .jpeg">
+                                <input type="file" name = "mobilePhoto" accept =".jpg, .png, .jpeg">
 
                                 <label class="form-label d-block mt-2">Brand</label>
-                                <input type="text" class="form-control">
+                                <input type="text" name = "brand" class="form-control">
                                 
                                 <label class="form-label mt-2">Model</label>
-                                <input type="text" class="form-control">
+                                <input type="text" name = "model" class="form-control">
                                 
                                 <label class="form-label mt-2">Processor</label>
-                                <input type="text" class="form-control">
+                                <input type="text" name = "processor" class="form-control">
                                 
                                 <label class="form-label mt-2">RAM</label>
-                                <input type="text" class="form-control">
+                                <input type="text" name = "RAM" class="form-control">
+
+                                <label class="form-label mt-2">Storage</label>
+                                <input type="text" name = "storage" class="form-control">
 
                                 <label class="form-label mt-2">Other Specifications</label>
-                                <textarea class="form-control"></textarea>
+                                <textarea class="form-control" name = "specifications"></textarea>
+
+                                <label class="form-label mt-2">Price</label>
+                                <input type="text" name = "price" class="form-control">
 
                                 <input type="submit" value ="Add" class="btn btn-success text-light mt-3" style="background-color: #161b40; color#fff; border:1px solid #fff">
                             </form>
