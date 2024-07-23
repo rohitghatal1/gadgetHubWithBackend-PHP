93df52f4ffc57fd90b6c1ba6b52c7bd0255171e0
rohitghatal1 <rohitghatal@gmail.com>
2024-07-23 17:53:09 +0545

    spearated mobile cards from home page

--- frontend/index.php
+++ frontend/index.php
@@ -234,37 +234,12 @@ if (isset($_SESSION['user'])) {
         <h2 class="pt-3">Mobiles</h2>
           <!-- Swiper -->
             <div class="container">
-            <div class="swiper mySwiper" style = "height: 30rem">
+            <div class="swiper mySwiper" style = "height: 35rem">
               <div class="swiper-wrapper">
-                <?php 
-                  $getMobles = "SELECT * FROM mobiles";
-                  $fetchedMobileData = $conn->query($getMobles);
-                  if($fetchedMobileData->num_rows>0){
-                    while($mobileInfo = $fetchedMobileData->fetch_assoc()){
-                      $mobileId = $mobileInfo['MId'];
-                      ?>
-                      <div class="swiper-slide">
-                        <div class="card">
-                          <img src="<?php echo $mobileInfo['photoPath']?>" class="card-img-top" alt="Testimonial 1">
-                          <div class="card-body">
-                            <h5 class="card-title"><?php echo $mobileInfo['brand']?></h5>
-                            <p class="card-text">Rs<?php echo $mobileInfo['Mprice']?></p>
-                          </div>
-                          <div class="watchDetails">
-                            <h2><?php echo $mobileInfo['brand']?></h2>
-                            <p><strong>Model:</strong> <span><?php echo $mobileInfo['model']?></span></p>
-                            <p><strong>Processor:</strong> <span><?php echo $mobileInfo['processor']?></span></p>
-                            <p><strong>RAM:</strong> <span><?php echo $mobileInfo['RAM']?></span></p>
-                            <p><strong>Storage:</strong> <span><?php echo $mobileInfo['storage']?></span></p>
-                            <p><strong>Price:</strong> <span><?php echo $mobileInfo['Mprice']?></span></p>
-                            <p><strong>Other Details:</strong> <span><?php echo $mobileInfo['otherSpecs']?></span></p>
-                            <button class="addToCartBtn"><i class="fas fa-shopping-cart"></i> Add to Cart</button>
-                          </div>
-                        </div>
-                      </div>
-                    <?php }
-                  }
-                ?>
+
+                <!-- to display all mobies  -->
+                <?php require 'php/mobileCards.php'?>
+                
             </div>
             <div class="swiper-pagination" style="margin-top: -2rem"></div>
             </div>
---
+++ frontend/php/mobileCards.php
@@ -0,0 +1,29 @@
+<?php 
+                  $getMobles = "SELECT * FROM mobiles";
+                  $fetchedMobileData = $conn->query($getMobles);
+                  if($fetchedMobileData->num_rows>0){
+                    while($mobileInfo = $fetchedMobileData->fetch_assoc()){
+                      $mobileId = $mobileInfo['MId'];
+                      ?>
+                      <div class="swiper-slide">
+                        <div class="card">
+                          <img src="<?php echo $mobileInfo['photoPath']?>" class="card-img-top" alt="Testimonial 1">
+                          <div class="card-body">
+                            <h5 class="card-title"><?php echo $mobileInfo['brand']?></h5>
+                            <p class="card-text">Rs<?php echo $mobileInfo['Mprice']?></p>
+                          </div>
+                          <div class="watchDetails">
+                            <h2><?php echo $mobileInfo['brand']?></h2>
+                            <p><strong>Model:</strong> <span><?php echo $mobileInfo['model']?></span></p>
+                            <p><strong>Processor:</strong> <span><?php echo $mobileInfo['processor']?></span></p>
+                            <p><strong>RAM:</strong> <span><?php echo $mobileInfo['RAM']?></span></p>
+                            <p><strong>Storage:</strong> <span><?php echo $mobileInfo['storage']?></span></p>
+                            <p><strong>Price:</strong> <span><?php echo $mobileInfo['Mprice']?></span></p>
+                            <p><strong>Other Details:</strong> <span><?php echo $mobileInfo['otherSpecs']?></span></p>
+                            <button class="addToCartBtn"><i class="fas fa-shopping-cart"></i> Add to Cart</button>
+                          </div>
+                        </div>
+                      </div>
+                    <?php }
+                  }
+                ?>
