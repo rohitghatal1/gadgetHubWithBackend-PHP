49c6691d60d071846f79d78d1ef4baafe12b3bce
Rohit Ghatal <109578725+rohitghatal1@users.noreply.github.com>
2024-11-23 22:22:38 +0545

    added count updater

--- frontend/php/cartSection.php
+++ frontend/php/cartSection.php
@@ -2,7 +2,6 @@
 // Include the database connection file
 require '../backend/database/databaseConnection.php';
 
-// Initialize cart items count
 $allCartItems = 0;
 $userName = "";
 // Check if the user is logged in
@@ -70,30 +69,30 @@ if (isset($_SESSION['user'])) {
                             $itemPrice = floatval($cartData['itemPrice']); // Convert price to float
                             $totalPrice += $itemPrice; // Add item price to total
                 ?>
-                            <tr>
-                                <td><?php echo $cartItemsCount; ?></td>
-                                <td><?php echo htmlspecialchars($cartData['itemBrand']); ?></td>
-                                <td><?php echo htmlspecialchars($cartData['itemModel']); ?></td>
-                                <td><img src="<?php echo htmlspecialchars($cartData['itemPhoto']); ?>" alt=""
-                                        style="width:80%; height:8rem;"></td>
-                                <td><?php echo number_format($itemPrice, 2); ?></td>
-                                <td>
-                                    <a href="php/removeFromCart.php?itemId=<?php echo urlencode($itemId); ?>&itemType=<?php echo urlencode($itemType); ?>"
-                                        class="text-decoration-none p-1 bg-danger text-light fw-bold rounded">
-                                        <i class="fas fa-trash"></i> Remove
-                                    </a>
-                                </td>
-                            </tr>
+                <tr>
+                    <td><?php echo $cartItemsCount; ?></td>
+                    <td><?php echo htmlspecialchars($cartData['itemBrand']); ?></td>
+                    <td><?php echo htmlspecialchars($cartData['itemModel']); ?></td>
+                    <td><img src="<?php echo htmlspecialchars($cartData['itemPhoto']); ?>" alt=""
+                            style="width:80%; height:8rem;"></td>
+                    <td><?php echo number_format($itemPrice, 2); ?></td>
+                    <td>
+                        <a href="php/removeFromCart.php?itemId=<?php echo urlencode($itemId); ?>&itemType=<?php echo urlencode($itemType); ?>"
+                            class="text-decoration-none p-1 bg-danger text-light fw-bold rounded">
+                            <i class="fas fa-trash"></i> Remove
+                        </a>
+                    </td>
+                </tr>
                 <?php
                             $cartItemsCount++;
                         }
                 ?>
-                        <!-- Display the total price -->
-                        <tr>
-                            <td colspan="4" style="text-align: right; font-weight: bold;">Total:</td>
-                            <td><?php echo number_format($totalPrice, 2); ?></td>
-                            <td></td>
-                        </tr>
+                <!-- Display the total price -->
+                <tr>
+                    <td colspan="4" style="text-align: right; font-weight: bold;">Total:</td>
+                    <td><?php echo number_format($totalPrice, 2); ?></td>
+                    <td></td>
+                </tr>
                 <?php
                     } else {
                         echo "<tr><td colspan='6'>No items added to cart</td></tr>";
